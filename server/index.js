92329a6e768cd3382ef108c7b5e537c24ca1a855
Ben Thornton <smenjas@users.noreply.github.com>
2021-02-20 18:00:18 -0800

    Refactor out routes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Under construction.');
-});
+require('./routes/main.routes.js')(app);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
